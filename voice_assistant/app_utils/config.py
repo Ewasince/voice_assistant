2bd0eb6db214a41cb39070005cedced610a7af58
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-05 21:47:25 +0400

    ref! config

--- voice_assistant/app_utils/config.py
+++ voice_assistant/app_utils/config.py
@@ -1,37 +1,13 @@
 import os
 
 import pyaudio
-from pydantic import BaseModel, Field
+from pydantic import BaseSettings, Field
 
 
-class Config(BaseModel):
+class Config(BaseSettings):
     """
     Основной класс конфига, выполняющий проверку всех полей
     """
-
-    def __new__(cls):
-        if hasattr(cls, "_instance"):
-            return cls._instance
-
-        # создание конфига
-        config_dict = {}
-
-        for param in cls.model_fields:
-            param: str
-            var = os.environ.get(param.upper())
-            if var is not None:
-                config_dict[param] = var
-
-        cls._instance = super(Config, cls).__new__(cls)
-        super(Config, cls._instance).__init__(**config_dict)
-
-        return cls._instance
-
-    # noinspection PyMissingConstructor
-    def __init__(self, *args, **kwargs):
-        pass
-
-    ####################
 
     # secrets
 
@@ -48,9 +24,3 @@ class Config(BaseModel):
     key_phase: str = Field(default="папич")
     regexp: str = Field(default=r"[^A-Za-zА-ЯЁа-яё0-9 ]")
     language: str = "ru-RU"
-
-    # делаем конфиг неизменяемым
-    class Config:
-        frozen = True
-
-    pass

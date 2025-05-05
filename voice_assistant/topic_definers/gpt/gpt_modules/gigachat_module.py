44cf52640189a0386544c1fc6f6d4d86cd8043a3
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-05 22:02:36 +0400

    improve gigachat

--- voice_assistant/topic_definers/gpt/gpt_modules/gigachat_module.py
+++ voice_assistant/topic_definers/gpt/gpt_modules/gigachat_module.py
@@ -1,25 +1,38 @@
-from gigachat import GigaChat
+from typing import ClassVar
+
+from voice_assistant.app_utils.config import Settings
+
+from langchain_gigachat.chat_models import GigaChat
+from langchain_core.messages import HumanMessage, SystemMessage
 
 from voice_assistant.topic_definers.gpt.gpt_modules.i_gpt_module import IGPTModule
 
 
 class GigaChatModule(IGPTModule):
-    def __init__(self, credentials: str):
-        self.credential = credentials
+    SCOPE: ClassVar[str] = "GIGACHAT_API_PERS"
+    MODEL: ClassVar[str] = "GigaChat-2"
+    SYSTEM_PROMPT: ClassVar[str] = "Ты — эксперт-лингвист, отлично разбирающийся в словообразовании, орфографии и значении слов, включая сленг, просторечие и разговорные формы. Твоя задача — преобразовать любое данное слово или выражение в его нормативную (литературную) форму: корректное слово, в правильном числе, падеже и без искажений. Отвечай кратко, точно и по делу — только нормализованная форма слова без пояснений."
+
+    def __init__(self, settings: Settings):
+        self._giga = GigaChat(
+            model=self.MODEL,
+            credentials=settings.gigachat_token,
+            scope=self.SCOPE,
+            verify_ssl_certs=False,
+            profanity_check=True
+        )
 
     def get_answer(self, text) -> str:
-        # Используйте токен, полученный в личном кабинете из поля Авторизационные данные
-        with GigaChat(credentials=self.credential, verify_ssl_certs=False) as giga:
-            response = giga.chat(text)
-            text = response.choices[0].message.content
-
-            return text
-
+        resp = self._giga.invoke(
+            [
+                SystemMessage(content=self.SYSTEM_PROMPT),
+                HumanMessage(content=text)
+            ]
+        )
+        return resp.content
 
 if __name__ == "__main__":
-    gc = GigaChatModule(input())
+    gc = GigaChatModule(Settings())
 
     while True:
         print(gc.get_answer(input()))
-
-    pass

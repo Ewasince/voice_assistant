8fec72c61afcad631754779204cf96239f993c89
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-08 21:05:25 +0400

    add cli iterator

---
+++ voice_assistant/commands_iterators/cli_iterator.py
@@ -0,0 +1,10 @@
+from voice_assistant.app_interfaces.command_iterator import CommandIterator
+from voice_assistant.app_utils.settings import Settings
+
+
+class TextSourceCLI(CommandIterator):
+    def __init__(self, settings: Settings):
+        self.config = settings
+
+    async def __anext__(self) -> str:
+        return input("Текстовая команда> ")

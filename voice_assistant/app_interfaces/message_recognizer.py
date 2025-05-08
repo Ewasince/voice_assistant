82b4b56d96c304f4c304bc7656638e831bd39e19
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-08 21:03:43 +0400

    ref! add CommandIterator interface

--- voice_assistant/app_interfaces/message_recognizer.py
+++ voice_assistant/app_interfaces/message_recognizer.py
@@ -1,14 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Self
-
-
-class CommandIterator(ABC):
-    async def __aiter__(self) -> Self:
-        return self
-
-    @abstractmethod
-    async def __anext__(self) -> str:
-        raise NotImplementedError
 
 
 

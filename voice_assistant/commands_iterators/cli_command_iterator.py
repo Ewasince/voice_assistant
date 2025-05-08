7a7a48c4f604cf7d7c249fb516b96bbdc921919c
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-08 21:07:01 +0400

    ref! add cli iterator

--- voice_assistant/commands_iterators/cli_command_iterator.py
+++ voice_assistant/commands_iterators/cli_command_iterator.py
@@ -2,7 +2,7 @@ from voice_assistant.app_interfaces.command_iterator import CommandIterator
 from voice_assistant.app_utils.settings import Settings
 
 
-class TextSourceCLI(CommandIterator):
+class CLICommandIterator(CommandIterator):
     def __init__(self, settings: Settings):
         self.config = settings
 

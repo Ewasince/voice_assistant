08fe23f7d8b54787d3c50d1025e20fe7eb3df6fc
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-08 22:32:42 +0400

    remove unused prompt

--- voice_assistant/topic_definers/gpt/gpt_modules/gigachat_module.py
+++ voice_assistant/topic_definers/gpt/gpt_modules/gigachat_module.py
@@ -1,6 +1,6 @@
 from typing import ClassVar
 
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage
 from langchain_gigachat.chat_models import GigaChat
 
 from voice_assistant.app_interfaces.gpt_module import LLMModule
@@ -10,12 +10,12 @@ from voice_assistant.app_utils.settings import Settings
 class GigaChatModule(LLMModule):
     SCOPE: ClassVar[str] = "GIGACHAT_API_PERS"
     MODEL: ClassVar[str] = "GigaChat-2"
-    SYSTEM_PROMPT: ClassVar[str] = (
-        "Ты — эксперт-лингвист, отлично разбирающийся в словообразовании, орфографии и значении слов, включая сленг, "
-        "просторечие и разговорные формы. Твоя задача — преобразовать любое данное слово или выражение в его "
-        "нормативную (литературную) форму: корректное слово, в правильном числе, падеже и без искажений. "
-        "Отвечай кратко, точно и по делу — только нормализованная форма слова без пояснений."
-    )
+    # SYSTEM_PROMPT: ClassVar[str] = (
+    #     "Ты — эксперт-лингвист, отлично разбирающийся в словообразовании, орфографии и значении слов, включая сленг, "
+    #     "просторечие и разговорные формы. Твоя задача — преобразовать любое данное слово или выражение в его "
+    #     "нормативную (литературную) форму: корректное слово, в правильном числе, падеже и без искажений. "
+    #     "Отвечай кратко, точно и по делу — только нормализованная форма слова без пояснений."
+    # )
 
     def __init__(self, settings: Settings):
         self._giga = GigaChat(
@@ -27,7 +27,7 @@ class GigaChatModule(LLMModule):
         )
 
     def get_answer(self, text) -> str:
-        resp = self._giga.invoke([SystemMessage(content=self.SYSTEM_PROMPT), HumanMessage(content=text)])
+        resp = self._giga.invoke([HumanMessage(content=text)])
         return resp.content
 
 

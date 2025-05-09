c7ae67d791f0d747c367e7705b6a41ea09edc55d
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-09 15:40:29 +0400

    ref! change time format

--- voice_assistant/commands/test_commands/get_current_time.py
+++ voice_assistant/commands/test_commands/get_current_time.py
@@ -10,4 +10,4 @@ class CommandGetCurrentTime(CommandPerformer):
     async def perform_command(self, *args: Any) -> str | None:
         # extract_text_after_command(command_text, self.get_command_text())
         cur_time = datetime.now()  # noqa: DTZ005
-        return f"Сейчас время: {cur_time}"
+        return f"Сейчас время: {cur_time.strftime('%H:%M:%S')}"

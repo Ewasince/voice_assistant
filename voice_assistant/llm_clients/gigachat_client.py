dd4c0f8e669bdadefaf44b2511b16aa64a4b9a2c
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-09 15:42:27 +0400

    ref! add retry to llm

--- voice_assistant/llm_clients/gigachat_client.py
+++ voice_assistant/llm_clients/gigachat_client.py
@@ -1,7 +1,10 @@
 from typing import ClassVar
 
+import httpx
 from langchain_core.messages import HumanMessage
 from langchain_gigachat.chat_models import GigaChat
+from loguru import logger
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_exponential
 
 from voice_assistant.app_interfaces.llm_module import LLMClient
 from voice_assistant.app_utils.settings import Settings
@@ -26,6 +29,15 @@ class GigaChatClient(LLMClient):
             profanity_check=True,
         )
 
+    @retry(
+        retry=retry_if_exception_type(httpx.ConnectError),
+        stop=stop_after_attempt(5),
+        wait=wait_exponential(multiplier=1, min=1, max=10),
+        before_sleep=lambda retry_state: logger.debug(
+            f"Fail to request llm #{retry_state.attempt_number}, retry in "
+            f"{retry_state.next_action.sleep if retry_state.next_action else 0:.1f}"
+        ),
+    )
     def get_simple_answer(self, text: str) -> str:
         resp = self._giga.invoke([HumanMessage(content=text)])
         content = resp.content

554445b9d165c7a6f9262def5cc44b6cabbc5919
ewasince <ewasince@gmail.com>
2025-07-20 21:32:03 +0300

    add time keeper integration with google calendar

--- voice_assistant/commands/time_keeper.py
+++ voice_assistant/commands/time_keeper.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import ClassVar, Final
 
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from loguru import logger
 
 from voxmind.app_interfaces.command_performer import CommandPerformer
@@ -34,6 +36,8 @@ _DEFINE_TASK_TOPIC_PROMPT: Final[str] = """\
 Не добавляй лишнего текста — только суть активности.
 """
 
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
 
 @dataclass
 class Contex:
@@ -50,6 +54,13 @@ class CommandTimeKeeperGoogle(CommandPerformer):
 
     def __init__(self, gpt_module: LLMClient):
         self._llm_module = gpt_module
+
+        # Авторизация
+        flow = InstalledAppFlow.from_client_secrets_file("../.cache/credentials.json", SCOPES)
+        creds = flow.run_local_server(port=0)
+
+        # Создание сервиса
+        self._service = build("calendar", "v3", credentials=creds)
 
     async def perform_command(self, command_text: str, context: GeneralContext) -> str | None:
         activity_type = self._llm_module.get_simple_answer(self._generate_define_activity_end_prompt(command_text))
@@ -121,6 +132,34 @@ class CommandTimeKeeperGoogle(CommandPerformer):
         end_time: datetime,
     ) -> None:
         logger.info(f"Бот записал активность {topic} с {start_time.strftime('%H:%M')} по {end_time.strftime('%H:%M')}")
+
+        # Данные мероприятия
+        event = {
+            "summary": topic,
+            # 'location': 'Москва, ул. Ленина, 1',
+            # 'description': 'Обсудим проект.',
+            "start": {
+                "dateTime": start_time.isoformat(),
+                "timeZone": "Europe/Moscow",
+            },
+            "end": {
+                "dateTime": end_time.isoformat(),
+                "timeZone": "Europe/Moscow",
+            },
+            # 'attendees': [
+            #     {'email': 'example@gmail.com'},
+            # ],
+        }
+
+        event = (
+            self._service.events()
+            .insert(
+                calendarId="1390da56718a398ff28cdefb909fffc3aa55bc7aea60faafdbed20e5c9e2121a@group.calendar.google.com",
+                body=event,
+            )
+            .execute()
+        )
+        print("Мероприятие создано: {}".format(event.get("htmlLink")))
 
     def _generate_define_activity_end_prompt(self, command: str) -> str:
         return self._define_end_task_prompt.format(

e759f9754db3141a488f634e75a0eb5d7417f7ab
ewasince <ewasince@gmail.com>
2025-07-29 08:53:42 +0300

    change tz setting

--- mcp_server/al_modules/settings.py
+++ mcp_server/al_modules/settings.py
@@ -1,8 +1,9 @@
 from pathlib import Path
-from zoneinfo import ZoneInfo
 
 from pydantic import field_validator
 from pydantic_settings import BaseSettings
+from pytz import timezone
+from pytz.tzinfo import BaseTzInfo
 
 
 class ALSettings(BaseSettings):
@@ -15,12 +16,12 @@ class ALSettings(BaseSettings):
     calendar_token_file: Path = Path("creds/token.json")
     calendar_id: str
 
-    calendar_tz: ZoneInfo = ZoneInfo("Europe/Moscow")
+    calendar_tz: BaseTzInfo = timezone("Europe/Moscow")
 
     # noinspection PyNestedDecorators
     @field_validator("calendar_tz", mode="before")
     @classmethod
     def parse_timezone(cls, v):
-        if isinstance(v, ZoneInfo):
+        if isinstance(v, BaseTzInfo | timezone):
             return v
-        return ZoneInfo(v)
+        return timezone(v)

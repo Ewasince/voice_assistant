0cbb56bdc819da4a3c11758c97c8f2a0b4d04112
ewasince <ewasince@gmail.com>
2025-07-31 19:43:12 +0300

    ref! rearrange

---
+++ voice_assistant/command_processer.py
@@ -0,0 +1,48 @@
+from typing import Any, Final
+
+import requests
+from loguru import logger
+
+from voice_assistant.settings import VASettings
+
+LANGFLOW_FLOW_URL_TEMPLATE: Final[str] = "http://localhost:7860/api/v1/run/{}?stream=false"
+settings = VASettings()
+
+
+async def process_command(command_text: str) -> str:
+    command_result = make_langflow_request(command_text)
+
+    assistant_response = f"Ответ ассистента > {command_result}"
+
+    if command_result is not None:
+        print(assistant_response)
+    else:
+        print()
+
+    return command_result
+
+
+def make_langflow_request(input_text: str) -> str:
+    payload = {
+        "output_type": "chat",
+        "input_type": "chat",
+        "input_value": input_text,
+        "session_id": settings.langflow_session_id,
+    }
+
+    headers: dict[str, Any] = {}
+
+    # Отправка POST-запроса
+    response = requests.post(
+        LANGFLOW_FLOW_URL_TEMPLATE.format(settings.langflow_flow_id), json=payload, headers=headers, timeout=100
+    )
+
+    data = response.json()
+
+    try:
+        text_response = data["outputs"][0]["outputs"][0]["outputs"]["message"]["message"]  # пиздец конечно
+    except (KeyError, IndexError):
+        logger.error(f"{data=}")
+        return "Ошибка парсинга ответа от langflow"
+
+    return text_response
--- voice_assistant/main.py
+++ voice_assistant/main.py
@@ -1,53 +1,27 @@
 import asyncio
 import sys
-from typing import Any, Final, NoReturn
+from typing import NoReturn
 
-import requests
 from dotenv import load_dotenv
 from loguru import logger
 from plyer import notification
 
-from voice_assistant.settings import VASettings
+from voice_assistant.command_processer import process_command, settings
 from voice_assistant.sources import get_local_source, get_tg_source
 from voxmind.app_interfaces.command_source import CommandSource
 
 logger.remove()  # Удаляем стандартный вывод в stderr
 logger.add(sys.stdout, level="DEBUG")  # Добавляем вывод в stdout
 
-LANGFLOW_FLOW_URL_TEMPLATE: Final[str] = "http://localhost:7860/api/v1/run/{}?stream=false"
-
-settings = VASettings()
-
 
 async def main() -> NoReturn:
     load_dotenv()
 
-    command_sources: list[CommandSource] = [
-        get_local_source(settings),
-        await get_tg_source(settings),
-    ]
+    command_sources: list[CommandSource] = await get_sources()
+
+    startup_completed()
 
     tasks = {asyncio.create_task(source.get_command()): n for n, source in enumerate(command_sources)}
-
-    async def process_command(command_text: str) -> str:
-        command_result = make_langflow_request(command_text)
-
-        assistant_response = f"Ответ ассистента > {command_result}"
-
-        if command_result is not None:
-            print(assistant_response)
-        else:
-            print()
-
-        return command_result
-
-    notification.notify(
-        title="Ассистент запущен",
-        message="Ассистент запущен",
-        app_name="Голосовой помощник",
-        timeout=10,  # в секундах
-    )
-
     while True:
         done, _ = await asyncio.wait(tasks.keys(), return_when=asyncio.FIRST_COMPLETED)
 
@@ -70,30 +44,20 @@ async def main() -> NoReturn:
     sys.exit(1)  # Завершение программы с кодом ошибки
 
 
-def make_langflow_request(input_text: str) -> str:
-    payload = {
-        "output_type": "chat",
-        "input_type": "chat",
-        "input_value": input_text,
-        "session_id": settings.langflow_session_id,
-    }
+async def get_sources() -> list[CommandSource]:
+    return [
+        get_local_source(settings),
+        await get_tg_source(settings),
+    ]
 
-    headers: dict[str, Any] = {}
 
-    # Отправка POST-запроса
-    response = requests.post(
-        LANGFLOW_FLOW_URL_TEMPLATE.format(settings.langflow_flow_id), json=payload, headers=headers, timeout=100
+def startup_completed() -> None:
+    notification.notify(
+        title="Ассистент запущен",
+        message="Ассистент запущен",
+        app_name="Голосовой помощник",
+        timeout=10,  # в секундах
     )
-
-    data = response.json()
-
-    try:
-        text_response = data["outputs"][0]["outputs"][0]["outputs"]["message"]["message"]  # пиздец конечно
-    except (KeyError, IndexError):
-        logger.error(f"{data=}")
-        return "Ошибка парсинга ответа от langflow"
-
-    return text_response
 
 
 if __name__ == "__main__":

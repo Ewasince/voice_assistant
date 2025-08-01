c8c61e6785f815f3f4469b3174dcb11f220fa393
ewasince <ewasince@gmail.com>
2025-08-01 20:57:36 +0300

    refactor

--- voice_assistant/services/memory.py
+++ voice_assistant/services/memory.py
@@ -1,31 +1,14 @@
-from dataclasses import dataclass
-from datetime import datetime
-
-from sqlalchemy import create_engine
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
-from voice_assistant.settings import va_settings
-from voice_assistant.tools.activity_logger.al_modules.schema import Base, ContexModel
-
-
-@dataclass
-class Contex:
-    last_activity_topic: str | None = None
-    last_activity_time: datetime | None = None
+from voice_assistant.database.core import engine
+from voice_assistant.database.models import Contex
+from voice_assistant.database.schema import ContexModel
 
 
 class MemoryService:
-    def __init__(self) -> None:
-        self._engine = create_engine(va_settings.database_uri, echo=False)
-
-        self.ensure_db()
-
-    def ensure_db(self) -> None:
-        Base.metadata.create_all(self._engine)
-
     def save_contex(self, contex: Contex, record_id: int = 1) -> None:
-        with Session(self._engine) as session:
+        with Session(engine) as session:
             try:
                 existing = session.query(ContexModel).filter_by(id=record_id).one()
                 existing.last_activity_topic = contex.last_activity_topic
@@ -40,7 +23,7 @@ class MemoryService:
             session.commit()
 
     def load_contex(self, record_id: int = 1) -> Contex:
-        with Session(self._engine) as session:
+        with Session(engine) as session:
             try:
                 db_obj = session.query(ContexModel).filter_by(id=record_id).one()
                 return Contex(

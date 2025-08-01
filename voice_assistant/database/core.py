0f61e01b779378d81e3c55fa0e54d8bad9fcfc84
ewasince <ewasince@gmail.com>
2025-08-01 21:22:38 +0300

    add first migration

--- voice_assistant/database/core.py
+++ voice_assistant/database/core.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 
-from voice_assistant.database.schema import Base
 from voice_assistant.settings import va_settings
 
 engine = create_engine(va_settings.database_uri, echo=False)
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)

a0aa570271057179d1749956881e11e234151d21
ewasince <ewasince@gmail.com>
2025-08-02 21:17:29 +0300

    ref! pref factory

--- voice_assistant/command_performers/performer_factory.py
+++ voice_assistant/command_performers/performer_factory.py
@@ -7,8 +7,8 @@ from voice_assistant.app_utils.types import CommandPerformerFunction, UserId
 async def get_performer(user_id: UserId) -> CommandPerformerFunction:
     logger.info(f"Initializing for user '{user_id}' command performer")
 
-    command_performer = CommandPerformer(user_id)
-    await command_performer.setup_agent()
+    agent = await get_agent(user_id)
+    command_performer = CommandPerformer(user_id, agent)
 
     return command_performer.perform_command  # TODO: will gc fights with it?
 
@@ -18,18 +18,11 @@ class CommandPerformer:
     Wraps agent request-response logic
     """
 
-    def __init__(self, user_id: UserId):
+    def __init__(self, user_id: UserId, agent: UserAgent):
         self._user_id = user_id
-
-        self._user_agent: UserAgent | None = None
-
-    async def setup_agent(self) -> None:
-        self._user_agent = await get_agent(self._user_id)
+        self._user_agent: UserAgent = agent
 
     async def perform_command(self, command_text: str) -> str | None:
-        if self._user_agent is None:
-            raise ValueError("Agent not initialized!")
-
         command_result = await self._user_agent.run_agent(command_text)
 
         if command_result is not None:

18d9630f1271b8cd5c79b3fe090b7c68658063ae
ewasince <ewasince@gmail.com>
2025-08-02 13:20:04 +0300

    add error wrap

--- voice_assistant/app_utils/base_settings.py
+++ voice_assistant/app_utils/base_settings.py
@@ -11,15 +11,18 @@ from pydantic_settings import (
 
 class ExtendedSource(EnvSettingsSource):
     def prepare_field_value(self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool) -> Any:
-        if field_name.lower().endswith("_list"):
-            return list(value.split(","))
-        if field_name.lower().endswith("_map"):
-            result_map = {}
-            for entry in value.split(";"):
-                key, value = entry.split(":")
-                result_map[key] = value
-            return result_map
-        return super().prepare_field_value(field_name, field, value, value_is_complex)
+        try:
+            if field_name.lower().endswith("_list"):
+                return list(value.split(","))
+            if field_name.lower().endswith("_map"):
+                result_map = {}
+                for entry in value.split(";"):
+                    key, value = entry.split(":")
+                    result_map[key] = value
+                return result_map
+            return super().prepare_field_value(field_name, field, value, value_is_complex)
+        except Exception as e:
+            raise ValueError from e
 
 
 class ExtendedSettings(BaseSettings, ABC):

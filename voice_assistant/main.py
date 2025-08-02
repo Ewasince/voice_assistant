1b24bbac5a6a02aa8a5469f0384209cfff75e0c9
ewasince <ewasince@gmail.com>
2025-08-02 12:21:15 +0300

    change message loop logic

--- voice_assistant/main.py
+++ voice_assistant/main.py
@@ -1,6 +1,7 @@
 import asyncio
+import contextlib
 import sys
-from typing import NoReturn
+from typing import AsyncGenerator, NoReturn
 
 from dotenv import load_dotenv
 from loguru import logger
@@ -8,7 +9,7 @@ from plyer import notification
 
 from voice_assistant.app_interfaces.command_source import CommandSource
 from voice_assistant.app_utils.settings import primary_settings
-from voice_assistant.app_utils.types import DEFAULT_USER_ID
+from voice_assistant.app_utils.types import DEFAULT_USER_ID, UserId
 from voice_assistant.command_processer import process_command
 from voice_assistant.command_sources.sources import get_sources
 
@@ -22,18 +23,29 @@ async def main() -> NoReturn:
     logger.info("Starting voice assistant")
 
     sources_to_use = primary_settings.sources_to_use_list
-    command_sources = await get_sources(DEFAULT_USER_ID, sources_to_use)
+
+    sources_by_users = {
+        user_id: await get_sources(user_id, sources_to_use)
+        for user_id in [
+            DEFAULT_USER_ID,
+        ]
+    }
+
+    # noinspection PyUnreachableCode
+    loops = [
+        asyncio.create_task(message_loop(user_id, command_sources))
+        for user_id, command_sources in sources_by_users.items()
+    ]
 
     startup_completed()
 
-    await message_loop(command_sources)
+    await asyncio.wait(loops, return_when=asyncio.ALL_COMPLETED)
 
-    # noinspection PyUnreachableCode
-    sys.exit(1)
+    sys.exit(0)
 
 
 def startup_completed() -> None:
-    logger.info("Startup completed!")
+    logger.success("Startup completed!")
     notification.notify(
         title="Ассистент запущен",
         message="Ассистент запущен",
@@ -42,23 +54,29 @@ def startup_completed() -> None:
     )
 
 
-async def message_loop(command_sources: list[CommandSource]) -> NoReturn:
-    tasks = {asyncio.create_task(source.get_command()): n for n, source in enumerate(command_sources)}
+async def message_loop(user_id: UserId, command_sources: list[CommandSource]) -> NoReturn:
+    logger.info(f"Start messages loop for user: {user_id}")
+    tasks = {asyncio.create_task(source.get_interaction_gen()): n for n, source in enumerate(command_sources)}
     while True:
         done, _ = await asyncio.wait(tasks.keys(), return_when=asyncio.FIRST_COMPLETED)
 
         for task in done:
             idx = tasks[task]
-            received_text = task.result()
+            interaction_gen: AsyncGenerator[str, str] = task.result()
+
+            received_text = await anext(interaction_gen)
 
             generated_text = await process_command(received_text)
 
             completed_source = command_sources[idx]
 
             if generated_text:
-                await completed_source.send_response(generated_text)
+                with contextlib.suppress(StopAsyncIteration):
+                    await interaction_gen.asend(generated_text)
+            else:
+                await interaction_gen.aclose()
 
-            new_task = asyncio.create_task(completed_source.get_command())
+            new_task = asyncio.create_task(completed_source.get_interaction_gen())
             del tasks[task]
             tasks[new_task] = idx
 

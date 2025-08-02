5dfdd244d5f0f03f477a916f7fc8c64675f4b7ed
ewasince <ewasince@gmail.com>
2025-08-02 17:54:25 +0300

    change memory service for session name

--- voice_assistant/services/memory.py
+++ voice_assistant/services/memory.py
@@ -1,33 +1,41 @@
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
+from voice_assistant.app_utils.types import UserId
 from voice_assistant.database.core import engine
 from voice_assistant.database.models import Contex
 from voice_assistant.database.schema import ContexModel
 
 
-class MemoryService:
-    def save_contex(self, contex: Contex, record_id: int = 1) -> None:
+class ContextMemoryService:
+    def __init__(self, user_id: UserId):
+        self._session_name = _get_session_name_from_user_id(user_id)
+
+    def save_contex(self, contex: Contex) -> None:
         with Session(engine) as session:
             try:
-                existing = session.query(ContexModel).filter_by(id=record_id).one()
+                existing = session.query(ContexModel).filter_by(session_name=self._session_name).one()
                 existing.last_activity_topic = contex.last_activity_topic
                 existing.last_activity_time = contex.last_activity_time
             except NoResultFound:
                 new_record = ContexModel(
-                    id=record_id,
+                    session_name=self._session_name,
                     last_activity_topic=contex.last_activity_topic,
                     last_activity_time=contex.last_activity_time,
                 )
                 session.add(new_record)
             session.commit()
 
-    def load_contex(self, record_id: int = 1) -> Contex:
+    def load_contex(self) -> Contex:
         with Session(engine) as session:
             try:
-                db_obj = session.query(ContexModel).filter_by(id=record_id).one()
+                db_obj = session.query(ContexModel).filter_by(session_name=self._session_name).one()
                 return Contex(
                     last_activity_topic=db_obj.last_activity_topic, last_activity_time=db_obj.last_activity_time
                 )
             except NoResultFound:
                 return Contex()
+
+
+def _get_session_name_from_user_id(user_id: UserId) -> str:
+    return f"session-{user_id}"

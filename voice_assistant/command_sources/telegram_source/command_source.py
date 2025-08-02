eb07fb760055b11208d4c29b51ffc1792a4749c7
ewasince <ewasince@gmail.com>
2025-08-02 12:21:07 +0300

    refactor telegram source for new architecture

--- voice_assistant/command_sources/telegram_source/command_source.py
+++ voice_assistant/command_sources/telegram_source/command_source.py
@@ -1,33 +1,59 @@
-import asyncio
+from asyncio import Queue
+from collections import namedtuple
+from functools import cache
+from typing import AsyncGenerator
 
 from loguru import logger
-from telegram import File, Update, Voice
+from telegram import Chat, File, Message, Update, User, Voice
 from telegram.ext import Application, ContextTypes, MessageHandler, filters
 
 from voice_assistant.app_interfaces.audio_recognizer import AudioRecognizer
 from voice_assistant.app_interfaces.command_source import CommandSource
-from voice_assistant.app_utils.settings import primary_settings
 from voice_assistant.app_utils.types import UserId
+from voice_assistant.command_sources.telegram_source.settings import telegram_settings
 from voice_assistant.command_sources.telegram_source.utils import get_audiodata_from_file
-from voice_assistant.sst_modules.sst_whisper import _get_whisper_sst_module
+from voice_assistant.sst_modules.sst_whisper import get_whisper_sst_module
+
+UserResponse = namedtuple("UserResponse", ["message_text", "chat_id"])
+
+type ResponseQuery = Queue[UserResponse]
 
 
 class TelegramBotCommandSource(CommandSource):
-    def __init__(self, user_id: UserId, sst_module: AudioRecognizer) -> None:
+    def __init__(
+        self,
+        user_id: UserId,
+        message_queue: ResponseQuery,
+        bot: Application,
+    ) -> None:
         super().__init__(user_id)
-        self._token = primary_settings.telegram_token
-        self._chat_id = primary_settings.telegram_chat_id
-        self._bot = Application.builder().token(self._token).build()
-        self._message_queue: asyncio.Queue[str] = asyncio.Queue()
+        self.message_queue = message_queue
+        self._bot = bot
 
-        self._sst_module = sst_module
+    async def _user_bot_interaction(self) -> AsyncGenerator[str | None, str]:
+        user_message_data = await self.message_queue.get()
+        yield None  # first yield None
+        response = yield user_message_data.message_text
+
+        await self._bot.bot.send_message(chat_id=user_message_data.chat_id, text=response)
+        logger.info(f"Answer for {self.user_id} sent to telegram: '{response}'")
 
     async def get_command(self) -> str:
-        return await self._message_queue.get()
+        raise NotImplementedError
 
     async def send_response(self, text: str) -> None:
-        await self._bot.bot.send_message(chat_id=self._chat_id, text=text)
-        logger.info(f"Answer sent to telegram: '{text}'")
+        raise NotImplementedError
+
+
+class TelegramBot:
+    def __init__(self, sst_module: AudioRecognizer) -> None:
+        self._sst_module = sst_module
+
+        self._bot = Application.builder().token(telegram_settings.telegram_token).build()
+
+        self._message_queues_by_users: dict[int, ResponseQuery] = {}
+
+        self.started = False
 
     async def start(self) -> None:
         self._bot.add_handler(
@@ -47,16 +73,37 @@ class TelegramBotCommandSource(CommandSource):
         await self._bot.initialize()
         await self._bot.start()
         await self._bot.updater.start_polling()  # type: ignore[union-attr]
+        self.started = True
         logger.info("Telegram bot started")
 
+    def get_source_for_user(self, user_id: UserId) -> TelegramBotCommandSource:
+        message_queue: ResponseQuery = Queue()
+
+        tg_user_id = telegram_settings.telegram_tg_users_to_ids_map[user_id]
+
+        self._message_queues_by_users[tg_user_id] = message_queue
+
+        return TelegramBotCommandSource(user_id, message_queue, self._bot)
+
     async def _handle_text_message(self, update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-        message_text = update.message.text  # type: ignore[union-attr]
-        if not isinstance(message_text, str):
-            raise ValueError(f"not suitable text: {message_text}")
-        await self._message_queue.put(message_text)
+        user_data = self._get_user_data(update)
+        if user_data is None:
+            return
+        message, user, chat, message_queue = user_data
+
+        message_text = message.text
+        if message_text is None:
+            return
+
+        await message_queue.put(UserResponse(message_text, chat.id))
 
     async def _handle_voice_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        voice: Voice = update.message.voice  # type: ignore[union-attr, assignment]
+        user_data = self._get_user_data(update)
+        if user_data is None:
+            return
+        message, user, chat, message_queue = user_data
+
+        voice: Voice = message.voice  # type: ignore[assignment]
         file_id = voice.file_id
         duration = voice.duration
 
@@ -66,14 +113,46 @@ class TelegramBotCommandSource(CommandSource):
 
         audio_data = await get_audiodata_from_file(voice_file)
 
-        res = await self._sst_module.recognize_from_audiodata(audio_data)
+        message_text = await self._sst_module.recognize_from_audiodata(audio_data)
 
-        await self._message_queue.put(res)
+        await message_queue.put(UserResponse(message_text, chat.id))
+
+    def _get_user_data(self, update: Update) -> tuple[Message, User, Chat, ResponseQuery] | None:
+        message = update.message
+        if message is None:
+            return None
+
+        from_user = message.from_user
+        if from_user is None:
+            return None
+
+        chat = message.chat
+        if chat is None:
+            return None
+
+        tg_user_id = from_user.id
+        tg_user_name = from_user.username
+
+        message_queue = self._message_queues_by_users.get(tg_user_id)
+        if message_queue is None:
+            logger.info(f"Bot has message from unknown tg user {tg_user_name} ({tg_user_id}): {message.text}")
+            return None
+
+        return message, from_user, chat, message_queue
+
+
+@cache
+def get_telegram_bot() -> TelegramBot:
+    audio_recognizer = get_whisper_sst_module()
+    return TelegramBot(audio_recognizer)
 
 
 async def get_tg_source(user_id: UserId) -> CommandSource:
-    audio_recognizer = _get_whisper_sst_module()
-    command_source = TelegramBotCommandSource(user_id, audio_recognizer)
-    await command_source.start()
+    telegram_bot = get_telegram_bot()
+
+    command_source = telegram_bot.get_source_for_user(user_id)
+
+    if not telegram_bot.started:
+        await telegram_bot.start()
 
     return command_source

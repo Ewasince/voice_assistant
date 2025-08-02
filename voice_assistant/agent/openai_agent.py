a821516403d06269b1af0f0c71cf8ec8e6cbf60f
ewasince <ewasince@gmail.com>
2025-08-02 15:29:35 +0300

    refactor agent getting

--- voice_assistant/agent/openai_agent.py
+++ voice_assistant/agent/openai_agent.py
@@ -1,31 +1,68 @@
-from agents import Agent, RunConfig, Runner, SQLiteSession
+from agents import Agent, RunConfig, Runner, SQLiteSession, Tool
 from agents.models.multi_provider import MultiProvider
 
 from voice_assistant.agent.settings import agent_settings
-from voice_assistant.tools.activity_logger.tools import log_end_activity, log_new_activity
-
-agent = Agent(
-    name="Personal AI assistant",
-    instructions="You are a helpful assistant who can use tools to answer questions and perform tasks.In addition to "
-    "simply using tools, you should also anticipate the needs of the person you are assisting. Tools are "
-    "not just for completing tasks—they are ways to help their owner, keep records, and make notes. You "
-    "should understand from the context which tool to use based on the potential desires or intentions of "
-    "the owner. Also, when the owner provides information about what they are doing, you should record "
-    "this activity using the appropriate tool.Answer in Russian",
-    tools=[
-        log_new_activity,
-        log_end_activity,
-    ],
-)
-
-session = SQLiteSession(agent_settings.agent_session_name)
+from voice_assistant.app_utils.types import UserId
+from voice_assistant.tools.tools import get_tools
 
 
-multi_provider = MultiProvider(
-    openai_base_url=agent_settings.agent_api_base_url,
-    openai_api_key=agent_settings.agent_api_key.get_secret_value(),
-    openai_use_responses=False,
-)
+class UserAgent:
+    def __init__(
+        self,
+        user_id: UserId,
+        tools: list[Tool],
+        # agent_name: str,
+        # agent_id: str,
+        # agent_settings: RunConfig,
+    ):
+        # self.agent_name = agent_name
+        # self.agent_id = agent_id
+        # self.agent_settings = agent_settings
+        # self.agent_settings.agent_
+
+        self._agent = Agent(
+            name="Personal AI assistant",
+            instructions="You are a helpful assistant who can use tools to answer questions and perform tasks. "
+            "In addition to simply using tools, you should also anticipate the needs of the person you "
+            "are assisting. Tools are not just for completing tasks—they are ways to help their owner, "
+            "keep records, and make notes. You should understand from the context which tool to use based "
+            "on the potential desires or intentions of the owner. Also, when the owner provides "
+            "information about what they are doing, you should record this activity using the appropriate "
+            "tool.Answer in Russian",
+            tools=tools,
+            # [
+            #     log_new_activity,
+            #     log_end_activity,
+            # ],
+        )
+
+        self._session = SQLiteSession(UserAgent.get_session_name(user_id))
+
+        # TODO: per-user models
+        self._multi_provider = MultiProvider(
+            openai_base_url=agent_settings.agent_api_base_url,
+            openai_api_key=agent_settings.agent_api_key.get_secret_value(),
+            openai_use_responses=False,
+        )
+
+        self._multi_provider._get_prefix_and_model_name = _get_prefix_and_model_name  # type: ignore[method-assign, assignment]
+
+    async def run_agent(self, input_text: str) -> str:
+        result = await Runner.run(
+            starting_agent=self._agent,
+            input=input_text,
+            run_config=RunConfig(
+                model=agent_settings.agent_model,
+                model_provider=self._multi_provider,
+                tracing_disabled=True,
+            ),
+            session=self._session,
+        )
+        return result.final_output
+
+    @staticmethod
+    def get_session_name(user_id: UserId) -> str:
+        return str(user_id)
 
 
 # need to compatibility with vsegpt
@@ -38,18 +75,6 @@ def _get_prefix_and_model_name(full_model_name: str | None) -> tuple[str | None,
     return None, full_model_name
 
 
-multi_provider._get_prefix_and_model_name = _get_prefix_and_model_name  # type: ignore[method-assign, assignment]
-
-
-async def run_agent(text: str) -> str:
-    result = await Runner.run(
-        starting_agent=agent,
-        input=text,
-        run_config=RunConfig(
-            model=agent_settings.agent_model,
-            model_provider=multi_provider,
-            tracing_disabled=True,
-        ),
-        session=session,
-    )
-    return result.final_output
+async def get_agent(user_id: UserId) -> UserAgent:
+    tools = await get_tools(user_id)
+    return UserAgent(user_id, tools)
--- voice_assistant/command_performers/performer.py
+++ voice_assistant/command_performers/performer.py
@@ -2,23 +2,43 @@ from typing import Awaitable, Callable
 
 from loguru import logger
 
-from voice_assistant.agent.openai_agent import run_agent
+from voice_assistant.agent.openai_agent import UserAgent, get_agent
 from voice_assistant.app_utils.types import UserId
 
 
 async def get_performer(user_id: UserId) -> Callable[[str], Awaitable[str]]:
     logger.info(f"Initializing for user '{user_id}' command performer")
-    return _process_command
+
+    command_performer = CommandPerformer(user_id)
+    await command_performer.setup_agent()
+
+    return command_performer.run_agent  # TODO: will gc fights with it?
 
 
-async def _process_command(command_text: str) -> str:
-    command_result = await run_agent(command_text)
+class CommandPerformer:
+    """
+    Wraps agent request-response logic
+    """
 
-    assistant_response = f"Ответ ассистента > {command_result}"
+    def __init__(self, user_id: UserId):
+        self._user_id = user_id
 
-    if command_result is not None:
-        print(assistant_response)
-    else:
-        print()
+        self._user_agent: UserAgent | None = None
 
-    return command_result
+    async def setup_agent(self) -> None:
+        self._user_agent = await get_agent(self._user_id)
+
+    async def run_agent(self, input_text: str) -> str:
+        if self._user_agent is None:
+            raise ValueError("Agent not initialized!")
+
+        command_result = await self._user_agent.run_agent(input_text)
+
+        assistant_response = f"Ответ ассистента > {command_result}"
+
+        if command_result is not None:
+            print(assistant_response)
+        else:
+            print()
+
+        return command_result

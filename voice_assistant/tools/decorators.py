963bcbf815148789ae6640238a72212475fb846f
ewasince <ewasince@gmail.com>
2025-08-05 16:24:48 +0300

    improove decorator

--- voice_assistant/tools/decorators.py
+++ voice_assistant/tools/decorators.py
@@ -1,16 +1,28 @@
 import functools
-from typing import Any, Awaitable, Callable, ParamSpec, TypeVar
+import sys
+import traceback
+from typing import Any, Awaitable, Callable, ParamSpec
 
 P = ParamSpec("P")
-R = TypeVar("R")
 
 
-def log_tool_decorator[**P, R](func: Callable[P, Awaitable[R]], logger: Any) -> Callable[P, Awaitable[R]]:
+def log_tool_decorator[**P](func: Callable[P, Awaitable[str]], logger: Any) -> Callable[P, Awaitable[str]]:
+    logger = logger.bind(action="tool")
+
     @functools.wraps(func)
-    async def wrapped(*args: P.args, **kwargs: P.kwargs) -> R:
-        logger.bind(action="tool").info(f"call '{func.__name__}' with args {args}, kwargs {kwargs} ")
-        res = await func(*args, **kwargs)
-        logger.bind(action="tool").info(f"done '{func.__name__}' with res {res}")
+    async def wrapped(*args: P.args, **kwargs: P.kwargs) -> str:
+        logger.info(f"CALL '{func.__name__}' with args {args}, kwargs {kwargs} ")
+        try:
+            res = await func(*args, **kwargs)
+        except Exception as exc:
+            logger.exception(f"FAIL call tool: {exc}")
+            traceback.print_exception(type(exc), exc, exc.__traceback__, file=sys.stderr)
+            raise exc
+
+        res = f"Сообщи пользователю что ты — {res}"
+
+        logger.info(f"DONE '{func.__name__}' with result: '{res}'")
+
         return res
 
     return wrapped

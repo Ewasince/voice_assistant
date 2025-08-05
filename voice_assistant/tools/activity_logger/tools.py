691cca0cd72fa16f3d8acbaaeb5f779dec8aaacf
ewasince <ewasince@gmail.com>
2025-08-05 16:26:16 +0300

    add tools

--- voice_assistant/tools/activity_logger/tools.py
+++ voice_assistant/tools/activity_logger/tools.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from agents import Tool
 
@@ -11,6 +11,7 @@ from voice_assistant.services.calendar.creds import get_calendar_credentials
 from voice_assistant.services.calendar.service import CalendarService
 from voice_assistant.services.calendar.settings import calendar_settings
 from voice_assistant.services.memory import ContextMemoryService
+from voice_assistant.tools.utils import parse_timedelta
 
 
 class ActivityLoggerToolset(Toolset):
@@ -27,55 +28,79 @@ class ActivityLoggerToolset(Toolset):
         return self._wrap_tools(
             self.log_new_activity,
             self.log_end_activity,
-            self.cancel_activity,
+            self.update_activity_in_memory,
+            self.forget_saved_activity,
+            self.get_saved_activity,
         )
 
     async def log_new_activity(
         self,
         new_activity_topic: str,
-        new_activity_offset: str | None = None,  # noqa: ARG002
+        new_activity_offset: str | None = None,
     ) -> str:
         """Log a new activity event.
-        Trigger this tool when the user indicates they are currently engaged in an activity or have just started one.
+        Trigger this tool when the user indicates they are currently engaged in an activity, have just started one,
+        or clearly stated that the activity began some time ago.
 
         Args:
             new_activity_topic: Name of the started activity. Should be short, precise, and sound natural to a human —
                 like a familiar, conventional label for the activity. Avoid synonyms, verb forms, or uncommon phrasings.
             new_activity_offset: Time since activity started (optional). Provide this only if the user explicitly
-                mentioned how long ago the activity began. The value must be in the %H:%M:%S format —
-                for example: "00:10:45" (10 minutes and 45 seconds ago).
+                mentioned how long ago the activity began.
+                The value must be in the %H:%M:%S format, а positive value indicates a delta into the past
+                (e.g., "00:10:45" means the activity started 10 minutes and 45 seconds ago).
         """
 
         context = self._memory_service.load_contex()
 
+        new_activity_delta = None
+        if new_activity_offset is not None:
+            new_activity_delta = parse_timedelta(new_activity_offset)
+
         response_message = ""
-        try:
-            current_time = datetime.now(tz=calendar_settings.calendar_tz)
+        current_time = datetime.now(tz=calendar_settings.calendar_tz)
 
-            last_activity_topic = context.last_activity_topic
-            last_activity_time = context.last_activity_time
-            if last_activity_topic and last_activity_time:
-                await self._calendar_service.jot_down_activity(
-                    last_activity_topic,
-                    last_activity_time,
-                    current_time,
+        end_time = current_time
+
+        if new_activity_delta is not None:
+            end_time = current_time + new_activity_delta
+
+        last_activity_topic = context.last_activity_topic
+        last_activity_time = context.last_activity_time
+        if last_activity_topic and last_activity_time:
+            if last_activity_time < end_time:
+                end_time = last_activity_time + timedelta(seconds=1)
+                response_message += (
+                    "Время окончания оказалось меньше времени начала! Поставил время на своё усмотрение. "
                 )
-                response_message += f'Зафиксировал активность "{last_activity_topic}". '
+            await self._calendar_service.jot_down_activity(
+                last_activity_topic,
+                last_activity_time,
+                end_time,
+            )
 
-            context.last_activity_topic = new_activity_topic
-            context.last_activity_time = current_time
+            new_activity_message = f'Зафиксировал конец активности "{last_activity_topic}"'
 
-            response_message += f'Запомнил активность "{new_activity_topic}". '
-        except Exception as e:
-            response_message += f"Возникла ошибка: {e}"
+            if new_activity_delta:
+                delta_minutes = new_activity_delta.seconds / 60
+                new_activity_message += f" {delta_minutes:.2f} минут назад. "
+            else:
+                new_activity_message += ". "
+
+            response_message += new_activity_message
+
+        context.last_activity_topic = new_activity_topic
+        context.last_activity_time = current_time
+
+        response_message += f'Запомнил активность "{new_activity_topic}". '
 
         self._memory_service.save_contex(context)
 
-        return f"Передай пользователю — {response_message}"
+        return response_message
 
     async def log_end_activity(
         self,
-        end_activity_offset: str | None = None,  # noqa: ARG002
+        end_activity_offset: str | None = None,
     ) -> str:
         """Log an activity event that ended.
         Use this tool only if the user has stated that an activity is finished AND has NOT mentioned being currently
@@ -83,57 +108,167 @@ class ActivityLoggerToolset(Toolset):
 
         Args:
             end_activity_offset: Time since activity ended (optional). Provide this only if the user explicitly
-                mentioned how long ago the activity ended. The value must be in the %H:%M:%S format —
-                for example: "00:10:45" (10 minutes and 45 seconds ago).
+                mentioned how long ago the activity ended.
+                The value must be in the %H:%M:%S format, а positive value indicates a delta into the past
+                (e.g., "00:10:45" means the activity started 10 minutes and 45 seconds ago).
         """
 
         context = self._memory_service.load_contex()
 
+        end_activity_delta = None
+        if end_activity_offset is not None:
+            end_activity_delta = parse_timedelta(end_activity_offset)
+
         response_message = ""
-        try:
-            current_time = datetime.now(tz=calendar_settings.calendar_tz)
+        current_time = datetime.now(tz=calendar_settings.calendar_tz)
 
-            last_activity_topic = context.last_activity_topic
-            last_activity_time = context.last_activity_time
+        end_time = current_time
 
-            if last_activity_topic and last_activity_time:
-                await self._calendar_service.jot_down_activity(
-                    last_activity_topic,
-                    last_activity_time,
-                    current_time,
+        if end_activity_delta is not None:
+            end_time = current_time + end_activity_delta
+
+        last_activity_topic = context.last_activity_topic
+        last_activity_time = context.last_activity_time
+
+        if last_activity_topic and last_activity_time:
+            if last_activity_time < end_time:
+                end_time = last_activity_time + timedelta(seconds=1)
+                response_message += (
+                    "Время окончания оказалось меньше времени начала! Поставил время на своё усмотрение. "
                 )
-            else:
-                self._logger.error(f"commit end activity called without last activity {context}")
+            await self._calendar_service.jot_down_activity(
+                last_activity_topic,
+                last_activity_time,
+                end_time,
+            )
+        else:
+            self._logger.error(f"commit end activity called without last activity {context}")
 
-            context.last_activity_topic = None
-            context.last_activity_time = None
+        context.last_activity_topic = None
+        context.last_activity_time = None
 
-            if last_activity_topic:
-                response_message += f'Зафиксировал конец активности "{last_activity_topic}"'
+        if last_activity_topic:
+            new_activity_message = f'Зафиксировал конец активности "{last_activity_topic}"'
+
+            if end_activity_delta:
+                delta_minutes = end_activity_delta.seconds / 60
+                new_activity_message += f" {delta_minutes:.2f} минут назад. "
             else:
-                response_message += "Не было информации о последней активности"
-        except Exception as e:
-            response_message += f"Возникла ошибка: {e}"
+                new_activity_message += ". "
+
+            response_message += new_activity_message
+        else:
+            response_message += "Не было информации о последней активности"
 
         self._memory_service.save_contex(context)
 
-        return f"Передай пользователю — {response_message}"
+        return response_message
 
-    async def cancel_activity(
+    async def update_activity_in_memory(
+        self,
+        activity_topic: str | None = None,
+        activity_relative_offset: str | None = None,
+        activity_offset_from_now: str | None = None,
+    ) -> str:
+        """Update details of a previously started activity with log_new_activity
+        Offset values must be in the %H:%M:%S format, а positive value indicates a delta into the past
+        (e.g., "00:10:45" means the activity started 10 minutes and 45 seconds ago).
+
+        Args:
+            activity_topic: (Optional) Updated name of the saved activity. Should be short, precise, and sound natural
+                to a human — like a familiar, conventional label for the activity. Avoid synonyms, verb forms, or
+                uncommon phrasings.
+                Provide only if the user wants to rename the remembered activity.
+
+            activity_relative_offset: (Optional) Relative shift to adjust the original
+                start time. Format must be "%H:%M:%S".
+                Use this only if the user wants to move the start time by a specific amount.
+
+            activity_offset_from_now: (Optional) Sets the start time to a point relative to the current time.
+                Format must be "%H:%M:%S".
+                Use this when the user indicates how long ago the activity started, relative to the present moment.
+
+        Notes:
+            - Only provide parameters that the user intends to change.
+            - Only one time-related parameter should be provided at a time. If more than one is given, the input is
+                considered invalid.
+        """
+        response_message = ""
+
+        context = self._memory_service.load_contex()
+        current_time = datetime.now(tz=calendar_settings.calendar_tz)
+
+        new_last_activity_time = None
+        if activity_relative_offset is not None and context.last_activity_time is not None:
+            new_last_activity_time = context.last_activity_time + parse_timedelta(activity_relative_offset)
+        elif activity_offset_from_now is not None:
+            new_last_activity_time = current_time + parse_timedelta(activity_offset_from_now)
+
+        if new_last_activity_time:
+            context.last_activity_time = new_last_activity_time
+            response_message += f"Перенёс активность на {new_last_activity_time.strftime('%H:%M')}. "
+
+        if activity_topic:
+            context.last_activity_topic = activity_topic
+            response_message += f"Переименовал активность на {activity_topic}. "
+
+        self._memory_service.save_contex(context)
+
+        return response_message
+
+    async def forget_saved_activity(
         self,
     ) -> str:
         """Use this tool when the user clearly states they want to cancel, delete, or forget an activity that was
-        previously stored. Do not use when the activity simply ends — use only if the user requests its removal
+        previously logged. Do not use when the activity simply ends — use only if the user requests its removal
         or negation.
         """
-        response = ""
+        response_message = ""
 
         context = self._memory_service.load_contex()
         self._memory_service.save_contex(Contex())
 
-        response += f"Отменил активность {context.last_activity_topic}"
+        response_message += f"Отменил активность {context.last_activity_topic}"
 
-        return f"Сообщи пользователю что ты — {response}"
+        return response_message
+
+    async def get_saved_activity(
+        self,
+    ) -> str:
+        """Retrieve information about the currently logged activity.
+        Use this tool when the user asks to view or recall what activity is currently saved in memory or what activity
+        is active.
+        This refers only to the explicitly stored activity via log_new_activity tool — not to general past mentions
+        in conversation.
+        """
+        response_message = ""
+
+        current_time = datetime.now(tz=calendar_settings.calendar_tz)
+
+        context = self._memory_service.load_contex()
+
+        last_activity_time = context.last_activity_time
+        last_activity_topic = context.last_activity_topic
+
+        if last_activity_time is None and last_activity_topic is None:
+            response_message += "Сейчас не запомнено активности"
+        elif last_activity_time is None:
+            response_message += f"Сейчас запомнена активность с темой '{last_activity_topic}'. "
+        elif last_activity_topic is None:
+            activity_delta = current_time - last_activity_time
+            delta_minutes = activity_delta.seconds / 60
+
+            response_message += f"Сейчас запомнена активность которая началась {delta_minutes:.2f} минут назад."
+        else:
+            activity_delta = current_time - last_activity_time
+            delta_minutes = activity_delta.seconds / 60
+
+            response_message += (
+                f"Сейчас запомнена активность с темой '{context.last_activity_topic}', "
+                f"которая началась {delta_minutes:.2f} минут назад."
+            )
+
+        return response_message
 
 
 async def get_activity_logger(user_id: UserId) -> ActivityLoggerToolset:
---
+++ voice_assistant/tools/utils.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+
+from dateutil import parser as dateutil_parser
+
+
+def parse_datetime(value: str) -> datetime:
+    """Преобразует строку ISO 8601 в datetime с таймзоной."""
+    return dateutil_parser.isoparse(value)
+
+
+def parse_timedelta(value: str) -> timedelta:
+    """
+    Преобразует строку в timedelta.
+    Поддерживает:
+      - ISO 8601 duration: PT15M, PT1H30M, -PT45M
+      - HH:MM:SS и -HH:MM:SS
+    """
+    value = value.strip()
+
+    if not value:
+        raise ValueError(f"parse_timedelta value {value!r} is empty")
+
+    # # ISO 8601 duration
+    # if value.upper().startswith("P") or value.upper().startswith("-P"):
+    #     negative = value.startswith("-")
+    #     if negative:
+    #         value = value[1:]  # убираем минус перед "P"
+    #     # простой парс через dateutil
+    #     duration = isoduration.parse_duration(value)
+    #     return -duration if negative else duration
+
+    # HH:MM:SS
+    # match = re.match(r"^(-)?(?:(\d+):)?(\d{1,2}):(\d{1,2})$", value)
+    # if match:
+    #     negative, hours, minutes, seconds = match.groups()
+    #     hours = int(hours or 0)
+    #     minutes = int(minutes)
+    #     seconds = int(seconds)
+    #     delta = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+    #     return -delta if negative else delta
+
+    negative = False
+    if value[0] in [
+        "−",
+        "-",
+        "‐",
+        "—",
+    ]:
+        negative = True
+        value = value[1:]
+
+    dt_offset = datetime.strptime(value, "%H:%M:%S")  # noqa: DTZ007
+    delta = timedelta(hours=dt_offset.hour, minutes=dt_offset.minute, seconds=dt_offset.second)
+
+    return delta * -1 if negative else delta

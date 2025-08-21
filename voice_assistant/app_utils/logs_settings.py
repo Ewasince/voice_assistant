74b33e6cc9e5a1f061affb7cc5ce507fb69865f3
ewasince <ewasince@gmail.com>
2025-08-21 23:25:32 +0300

    fix

--- voice_assistant/app_utils/logs_settings.py
+++ voice_assistant/app_utils/logs_settings.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from pydantic import field_validator
 from pydantic_settings import SettingsConfigDict
 
 from voice_assistant.app_utils.settings_utils.common import HierarchicalSettings
@@ -14,3 +15,9 @@ class LogsSettings(HierarchicalSettings):
     rotation: str | None = "20 MB"
     retention: int | None = 5
     compression: str | None = "zip"
+
+    # noinspection PyNestedDecorators
+    @field_validator("level", mode="before")
+    @classmethod
+    def parse_timezone(cls, v: str) -> str:
+        return v.upper()
